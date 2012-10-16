0e2c7a0400706e0511841f376637fea398d37b8f
Nejc Zupan <nejc.zupan@gmail.com>
2012-10-16 11:37:13 +0200

    refactoring interfaces.py

--- src/niteoweb/jvzoo/interfaces.py
+++ src/niteoweb/jvzoo/interfaces.py
@@ -4,22 +4,22 @@ Interfaces, Events and Exceptions
 ---------------------------------
 """
 
-from niteoweb.click2sell import Click2SellMessageFactory as _
+from niteoweb.jvzoo import JVZooMessageFactory as _
 from zope import schema
 from zope.interface import Attribute
 from zope.interface import implements
 from zope.interface import Interface
-from niteoweb.click2sell import parse_mapping
+from niteoweb.jvzoo import parse_mapping
 
 
 # control panel schema
-class IClick2SellSettings(Interface):
-    """Definition of fields for niteoweb.click2sell configuration form."""
+class IJVZooSettings(Interface):
+    """Definition of fields for niteoweb.jvzoo configuration form."""
 
     secretkey = schema.Password(
-        title=_(u"click2sell Secret Key"),
+        title=_(u"JVZoo Secret Key"),
         description=_(u"help_secretkey",
-            default=u"Enter the Secret Key you got from Click2Sell to access " \
+            default=u"Enter the Secret Key you got from JVZoo to access " \
                      "their API."),
         required=True,
     )
@@ -29,7 +29,7 @@ class IClick2SellSettings(Interface):
         description=_(u"help_secretkey",
             default=u"Optionally, you can set Product ID to Group mapping. " \
             "This is used to automatically add a new member to a certain " \
-            "group based on which Click2Sell product was purchased. Format: " \
+            "group based on which JVZoo product was purchased. Format: " \
             "'PRODUCT_ID|GROUP_ID'. Example: '1|premium_members'. One " \
             "mapping per line."),
         required=False,
@@ -39,27 +39,27 @@ class IClick2SellSettings(Interface):
 
 
 # exceptions
-class Click2SellError(Exception):
-    """Base class for niteoweb.click2sell exception."""
+class JVZooError(Exception):
+    """Base class for niteoweb.jvzoo exception."""
 
 
-class SecretKeyNotSet(Click2SellError):
-    """Exception thrown when secret-key for click2sell is not set."""
+class SecretKeyNotSet(JVZooError):
+    """Exception thrown when secret-key for jvzoo is not set."""
 
 
 # events
-class IClick2SellEvent(Interface):
-    """Base class for niteoweb.click2sell events."""
+class IJVZooEvent(Interface):
+    """Base class for niteoweb.jvzoo events."""
 
 
-class IMemberCreatedEvent(IClick2SellEvent):
+class IMemberCreatedEvent(IJVZooEvent):
     """Interface for MemberCreatedEvent."""
-    context = Attribute("A member was created by @@click2sell.")
+    context = Attribute("A member was created by @@jvzoo.")
 
 
 class MemberCreatedEvent(object):
-    """Emmited when a new member is created by click2sell post-back service
-    calling @@click2sell.
+    """Emmited when a new member is created by jvzoo post-back service
+    calling @@jvzoo.
     """
     implements(IMemberCreatedEvent)
 
